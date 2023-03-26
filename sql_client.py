b124f1f57654f7a2572993fc33fd0a4aa20ee4a7
Gyuri1 <Acs.gyorgy1@gmail.com>
2023-03-26 14:08:50 +0200

    Add files via upload

--- sql_client.py
+++ sql_client.py
@@ -10,17 +10,17 @@ def call_sql(text):
         print("Connection problem")
          
 for i in range(1000):
-  client= pymssql.connect("sql_server.mycompany.com:1433", "sqladmin", "BigSecret", "database_name")           
+  client= pymssql.connect("sql.mycompany.com:1433", "sqladmin", "bigsecret", "DBname")           
   cursor=client.cursor(as_dict=True)
  
-  print("PRINT ALL with SELECT *") 
-  call_sql("SELECT FROM TAJ:")
+  print("PRINT * ALL with SELECT *") 
+  call_sql("SELECT * FROM TAJ;")
 
   print("print good ID:")
-  call_sql("SELECT A FROM TAJ WHERE id='123456788';")
+  call_sql("SELECT * FROM TAJ WHERE taj='123456788';")
 
   print("print bad ID:")
-  call_sql("SELECT A FROM TAJ WHERE id='11234567891';")
+  call_sql("SELECT * FROM TAJ WHERE taj='123456789';")
 
   client. close() 
   time.sleep(3) 
